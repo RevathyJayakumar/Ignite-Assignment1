7fc3fa637b5f656af91e8bbce016a71429fcf05a
RevathyJayakumar <78840100+RevathyJayakumar@users.noreply.github.com>
2021-03-19 13:07:41 +0530

    Update Qn12.java

--- Qn12.java
+++ Qn12.java
@@ -1,1 +1,33 @@
+public class Qn12 {
+
+    public static void main(String[] args) {
+
+        int length = 16;
+        int height = 8;
+
+        for (int i = 0; i <= height; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.out.print("* ");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
 
